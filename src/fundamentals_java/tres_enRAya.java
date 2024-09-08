5933d8eb00b1c1e3d26f5f9675cd5b421e3e8e18
shadin <shadyndyn@gmail.com>
2024-09-08 22:06:24 +0200

    tres en raya empezado

--- src/fundamentals_java/tres_enRAya.java
+++ src/fundamentals_java/tres_enRAya.java
@@ -5,6 +5,20 @@ public class tres_enRAya {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		String [] tablero = {"_","_","_","_","_","_","_","_","_"};
+		showBoard(tablero);
+		
+	}
+	
+	public static  void showBoard(String [] tablero) {
+		
+		System.out.println(tablero[0]+ " | "+tablero[1]+ " | "+tablero[2]);
+		System.out.println(tablero[3]+ " | "+tablero[4]+ " | "+tablero[5]);
+		System.out.println(tablero[6]+ " | "+tablero[7]+ " | "+tablero[8]);
+ 
+		
+		
+		
 	}
 
 }
