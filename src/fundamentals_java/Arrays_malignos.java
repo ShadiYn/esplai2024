4f6080e14b0a4d3b510a5f157a63b3b96c87e04e
shadyn <shadyndyn234@gmail.com>
2024-09-09 15:41:36 +0200

    hecho el tres en raya

--- src/fundamentals_java/Arrays_malignos.java
+++ src/fundamentals_java/Arrays_malignos.java
@@ -4,15 +4,7 @@ public class Arrays_malignos {
 
 	public static void main(String[] args) {
 		
-		//imprimirDobleNumero();
-		
-		int [] iarray = toArray(10,20);
-		System.out.println("Array : "+iarray[0]+" "+iarray[1]);
-		System.out.println(getFirstElement(iarray));
-		int[] array = {10,20,30};
-		setFirstElement(array,5);
-        System.out.println("Array modificado: " + java.util.Arrays.toString(array));
-		System.out.println("ultimo elemento del array es: "+getLastElement(array));
+		imprimirDobleNumero();
 	}
 	
 	void imprimirNombres() {
@@ -32,41 +24,11 @@ public class Arrays_malignos {
 		}
 	}
 	
-	public static  int[] toArray(int n1, int n2) {
-		// Escribe una función toArray que reciba dos valores y devuelva un array con estos dos valores
-	
-		int [] array = new int[2];
-		array[0]=n1;
-		array[1]=n2;
+	public static void recibirValores() {
+		//ejercicio 3
+		int[] numeros = new int[2];
 		
-		return array;
-		
-		
-	}
-	
-	public static int getFirstElement(int[] array) {
-		if(array ==null || array.length==0) {
-			
-			System.out.println("El array esta vacio no se devovlerá nada");
-		}
-		return array[0];
-		
-	}
-	
-	public static int[] setFirstElement(int [] array, int numero) {
-		
-		array[0]=numero;
-		
-		return array;
-	}
-	
-	public static int getLastElement(int [] array) {
-		
-		if(array == null || array.length ==0) {
-			System.out.println("el array esta vacio");
-		}
-		
-		return array[array.length-1];
+		int n1,n2;
 		
 	}
 
--- src/fundamentals_java/tres_enRAya.java
+++ src/fundamentals_java/tres_enRAya.java
@@ -1,14 +1,47 @@
 package fundamentals_java;
+import java.util.Scanner;
 
 public class tres_enRAya {
+	
+
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-		String [] tablero = {"_","_","_","_","_","_","_","_","_"};
-		showBoard(tablero);
-		
+		String[] tablero = { "_", "_", "_", "_", "_", "_", "_", "_", "_" };
+		int tirada = 0;
+		boolean alguienGano = false;
+		do {
+			showBoard(tablero);
+			String ficha = tirada%2==0? "X":"O";
+			Jugador(tablero, ficha);
+			
+			
+			alguienGano = evaluarGanador(tablero);
+			if(alguienGano)System.out.println("Enhorabuena, player " + ficha);
+			System.out.println(tablero);
+			tirada++;
+		} while (!alguienGano);
+
+	
 	}
+	
+	/*
+	 * String[] tablero = { "_", "_", "_", "_", "_", "_", "_", "_", "_" };
+		int tirada = 0;
+		boolean alguienGano = false;
+		do {
+			showTablero(tablero);
+			String ficha = tirada%2==0? "X":"O";
+			juegaPlayer(ficha, tablero);
+			
+			
+			alguienGano = evaluateWin(tablero);
+			if(alguienGano)System.out.println("Enhorabuena, player " + ficha);
+			tirada++;
+		} while (!alguienGano);
+
+	}
+*/
 	
 	public static  void showBoard(String [] tablero) {
 		
@@ -19,6 +52,44 @@ public class tres_enRAya {
 		
 		
 		
+	}
+	
+	public static  String Jugador(String [] tablero, String ficha) {
+		Scanner sc = new Scanner (System.in);
+
+		System.out.println("Que posicion quieres moverte Jugador:  "+ficha);
+			int posicion = sc.nextInt();
+			
+			return tablero[posicion] = ficha;
+			
+			
+	}
+	
+	
+	static boolean evaluarGanador(String [] tablero ) {
+		if(tablero[0].equals(tablero[1]) && tablero[0].equals(tablero[2]) && !tablero[0].equals("_")) {
+			return true;
+		}
+		
+		else if(tablero[3].equals(tablero[4]) && tablero[3].equals(tablero[5]) && !tablero[3].equals("_")) {
+			return true;
+		}
+		else if(tablero[6].equals(tablero[7]) && tablero[6].equals(tablero[8]) && !tablero[6].equals("_")) {
+			return true;
+		}
+		else if(tablero[0].equals(tablero[3]) && tablero[0].equals(tablero[6]) && !tablero[0].equals("_")) {
+			return true;
+		}
+		
+		else if(tablero[1].equals(tablero[4]) && tablero[1].equals(tablero[7]) && !tablero[1].equals("_")) {
+			return true;
+		}
+		else if(tablero[2].equals(tablero[5]) && tablero[2].equals(tablero[8]) && !tablero[2].equals("_")) {
+			return true;
+		}
+		
+		return false;
+
 	}
 
 }
